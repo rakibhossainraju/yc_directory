157d89f17a201f9465f87d4f571aa3395c618854
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-11 16:52:42 +0600

    Remove unnecessary `antialiased` class from body element in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${workSans.variable} antialiased`}>{children}</body>
+      <body className={workSans.variable}>{children}</body>
     </html>
   );
 }
