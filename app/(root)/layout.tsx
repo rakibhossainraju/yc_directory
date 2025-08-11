2dd625f50379de475e7dca0df29c9e50c9be8e29
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-11 15:05:10 +0600

    Initialized the up and setup OAuth to login

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import { Navbar } from "@/app/components/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
+
+export const Navbar = async () => {
+  const session = await auth();
+  const user = session?.user;
+  const isLoggedIn = session !== null && user !== undefined;
+  console.log("user", user);
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="Site Logo" width={100} height={100} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {isLoggedIn ? (
+            <>
+              <Link href="/app/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
+              </form>
+              <Link href={`/user/${user.id}`}>
+                <span>{user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("git");
+                }}
+              >
+                <button type="submit">
+                  <span>Login</span>
+                </button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
