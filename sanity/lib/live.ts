ddbef4df51cae03859d9354ce85389df35b5811a
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-17 11:12:30 +0600

    Update live.ts to use `apiVersion` from environment and disable tokens in client configuration

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,11 +4,12 @@
 import { defineLive } from "next-sanity";
 import "server-only";
 import { client } from "./client";
+import { apiVersion } from "../env";
 
 export const { sanityFetch, SanityLive } = defineLive({
   client: client.withConfig({
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: "vX",
+    apiVersion,
   }),
+  serverToken: false,
+  browserToken: false,
 });
