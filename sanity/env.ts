1581c549fca81c49e972fc12be4fd60a55db5e51
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-17 18:09:02 +0600

    Add Sanity Write token environment variable and improve error handling

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,25 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-08-12'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-08-12";
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
+);
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+);
+
+export const sanityWriteToken = assertValue(
+  process.env.SANITY_WRITE_TOKEN,
+  "Missing Sanity Write token",
+);
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
