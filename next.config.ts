6101b587781d7dd1b94268274bd12366012fdc49
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-17 11:12:12 +0600

    Update `next.config.ts` with image handling, experimental features, and dev indicators configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  experimental: { ppr: "incremental" },
+  devIndicators: {
+    position: "bottom-right",
+  },
+  turbopack: {
+    root: "/home/light/Desktop/yc_directory",
+  },
 };
 
 export default nextConfig;
