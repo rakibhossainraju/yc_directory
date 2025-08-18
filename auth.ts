500d8fcbb52db3b3a7b647c5d5f9de39238bc94c
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-18 12:33:59 +0600

    Integrate user creation and update logic in NextAuth callbacks using Sanity write client.

--- auth.ts
+++ auth.ts
@@ -1,6 +1,41 @@
 import NextAuth from "next-auth";
 import GitHub from "@auth/core/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({ user: { email, image, name }, profile }) {
+      const { id, login, bio } = profile!;
+      const existingUser = await writeClient.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+        id: id,
+      });
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          email,
+          image,
+          name,
+          username: login,
+          bio: bio || "",
+        });
+      }
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await writeClient.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id: profile.id,
+        });
+        token.id = user?._id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
 });
