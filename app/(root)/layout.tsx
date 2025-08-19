b663acab85a76935f04614940c39fc5b1bf64d3d
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-19 12:54:02 +0600

    Wrap Navbar component in Suspense for improved loading state handling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
-import type { ReactNode } from "react";
+import { type ReactNode, Suspense } from "react";
 import { Navbar } from "@/components/Navbar";
+export const experimental_ppr = true;
 
 export default function Layout({
   children,
@@ -8,7 +9,15 @@ export default function Layout({
 }>) {
   return (
     <main>
-      <Navbar />
+      <Suspense
+        fallback={
+          <nav>
+            <h1>Loading</h1>
+          </nav>
+        }
+      >
+        <Navbar />
+      </Suspense>
       {children}
     </main>
   );
