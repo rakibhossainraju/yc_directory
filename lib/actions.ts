7dad9e1fb8c75b4b51cbbb2dae53ce6e0c6f94da
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-19 16:03:32 +0600

    Add `createStartup` server action for handling startup creation

---
+++ lib/actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import "server-only";
+import { auth } from "@/auth";
+import { isSessionValid } from "@/lib/server-utils";
+import { StartupData } from "@/components/StartupForm";
+import { writeClient } from "@/sanity/lib/write-client";
+import slugify from "slugify";
+
+export async function createStartup(formValues: StartupData) {
+  const session = await auth();
+  if (isSessionValid(session))
+    return { error: "Not signed in", status: "ERROR" };
+
+  try {
+    const slug = slugify(formValues.title, { lower: true, strict: true });
+    Object.assign(formValues, {
+      slug: { _type: slug, current: slug },
+      author: { _type: "reference", _ref: session?.id },
+    });
+    const res = await writeClient.create({ _type: "startup", ...formValues });
+    return { error: "", status: "SUCCESS", id: res._id };
+  } catch {
+    return { error: "Something went wrong", status: "ERROR" };
+  }
+}
