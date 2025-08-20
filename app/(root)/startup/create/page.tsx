4929662bdad2ae97aee52bd08d159afc8f288bac
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-20 14:25:03 +0600

    Refactor random number logic in `Page` component for improved readability

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -3,12 +3,13 @@ import StartupForm from "@/components/StartupForm";
 
 export const experimental_ppr = true;
 
-const Page = async () => {
+const Page = () => {
+  const rand = Math.floor(Math.random() * 99) + 1;
   return (
     <>
       <section className="pink_container pattern !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
-        <h1 className="heading">{Math.floor(Math.random() * 99) + 1}</h1>
+        <h1 className="heading">{rand}</h1>
       </section>
       <StartupForm />
     </>
