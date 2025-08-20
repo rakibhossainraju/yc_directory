9f840e23a9015d115934c0b7687cff2b8e1eb768
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-20 14:27:20 +0600

    Add `UserStartups` component to display startups by user ID with data fetching

---
+++ components/UserStartups.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { STARTUPS_BY_AUTHOR_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import StartupCard from "@/components/StartupCard";
+import { StartupTypeCard } from "@/app/(root)/page";
+
+const UserStartups = async ({ userId }: { userId: string }) => {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const startups = await client.fetch(STARTUPS_BY_AUTHOR_ID_QUERY, {
+    id: userId,
+  });
+  return (
+    <>
+      {startups.map((startup) => (
+        <StartupCard key={startup._id} startup={startup as StartupTypeCard} />
+      ))}
+    </>
+  );
+};
+export default UserStartups;
