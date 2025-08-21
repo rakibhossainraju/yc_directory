09b9c72566b9e069ae00a136a5bcb118c081778a
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-22 02:30:54 +0600

    Refactor Home component to improve query handling and enhance readability

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,19 @@
-import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
-import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { Startup, Author } from "@/sanity/types";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { Startup, Author } from '@/sanity/types';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
+import { Suspense } from 'react';
+import StartupCardSkeleton from '@/components/StartupCardSkeleton';
 
-export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+export type StartupTypeCard = Omit<Startup, 'author'> & { author?: Author };
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-  const query = (await searchParams).query ?? "";
-  const startups = (
-    await sanityFetch({
-      query: STARTUPS_QUERY,
-      params: { search: query || null },
-    })
-  ).data as StartupTypeCard[];
+  const query = (await searchParams).query ?? null;
 
   return (
     <>
@@ -26,25 +22,40 @@ export default async function Home({
           Pitch Your Startup, <br /> Connect With Entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-          Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
-        <SearchForm query={query} />
+        <SearchForm query={query ?? ''} />
       </section>
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results fro "${query}"` : "Recent Pitches"}
+          {query ? `Search results for "${query}"` : 'Recent Pitches'}
         </p>
         <ul className="mt-7 card_grid">
-          {startups.map((startup, index) => (
-            <StartupCard
-              key={(startup._id + index).toString()}
-              startup={startup}
-            />
-          ))}
+          <Suspense fallback={<StartupCardSkeleton count={6} />}>
+            <StartUpCards query={query} />
+          </Suspense>
         </ul>
-        <SanityLive />
+        <Suspense>
+          <SanityLive />
+        </Suspense>
       </section>
+    </>
+  );
+}
+
+async function StartUpCards({ query }: { query: string | null }) {
+  const startups = (
+    await sanityFetch({
+      query: STARTUPS_QUERY,
+      params: { search: query },
+    })
+  ).data as StartupTypeCard[];
+
+  return (
+    <>
+      {startups.map((startup, index) => (
+        <StartupCard key={(startup._id + index).toString()} startup={startup} />
+      ))}
     </>
   );
 }
