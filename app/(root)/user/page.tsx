4a77ac3c18cd6285b19168a4ae7a04f9a36389ec
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-21 17:55:10 +0600

    Add `user` page with session validation and redirect functionality

---
+++ app/(root)/user/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { isSessionInValid } from "@/lib/server-utils";
+
+const Page = async () => {
+  const session = await auth();
+  if (isSessionInValid(session)) {
+    redirect("/");
+  }
+  const id = session!.id!;
+  const user = session!.user!;
+
+  return (
+    <>
+      <div>{user.name}</div>
+    </>
+  );
+};
+export default Page;
