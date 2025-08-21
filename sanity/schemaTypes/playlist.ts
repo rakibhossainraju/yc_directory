a0149d18bdb490547cd8741a2d31b60ae383bd9c
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-21 17:54:50 +0600

    Add `playlist` schema to define playlists with title, slug, and selection fields

---
+++ sanity/schemaTypes/playlist.ts
@@ -0,0 +1,29 @@
+import { defineType, defineField } from "sanity";
+
+export const playlist = defineType({
+  name: "playlist",
+  title: "Playlist",
+  icon: () => "🎵",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "select",
+      type: "array",
+      of: [{ type: "reference", to: { type: "startup" } }],
+    }),
+  ],
+  preview: {
+    select: { title: "title" },
+  },
+});
