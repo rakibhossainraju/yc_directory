89c139951ab5a06a5a43334dd25de2fa18ccf8a5
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-22 23:41:34 +0600

    Rename RootLayout to AppLayout for improved clarity and consistency

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '@/components/Navbar';
 
 export const experimental_ppr = true;
 
-export default function RootLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: ReactNode;
