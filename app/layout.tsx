2197df55b271a23d94c19d99f4fa60a9461c240d
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-22 23:41:25 +0600

    Refactor layout component for improved consistency in import style and formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from "next";
-import { Work_Sans } from "next/font/google";
-import "easymde/dist/easymde.min.css";
-import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from 'next';
+import { Work_Sans } from 'next/font/google';
+import 'easymde/dist/easymde.min.css';
+import '../styles/globals.css';
+import '../styles/nprogress.css';
+import { Toaster } from '@/components/ui/sonner';
+import { HandleOnComplete } from '@/lib/custom-router';
 
 const workSans = Work_Sans({
-  variable: "--font-work-sans",
-  subsets: ["latin"],
+  variable: '--font-work-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "YC Directory",
-  description: "Pitch, Vote and Grow Your Startup",
+  title: 'YC Directory',
+  description: 'Pitch, Vote and Grow Your Startup',
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
 };
 
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html data-color-mode="light" lang="en">
       <body className={workSans.variable}>
+        <HandleOnComplete />
         {children}
         <Toaster richColors />
       </body>
