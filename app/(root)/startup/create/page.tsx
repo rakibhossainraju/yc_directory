11e0cc6a94e27d3860c859da3398b31d51f6c220
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-23 16:09:50 +0600

    Refactor `create` page: update imports to single quotes and remove unused random number generation

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import StartupForm from "@/components/StartupForm";
+import React from 'react';
+import StartupForm from '@/components/StartupForm';
 
 export const experimental_ppr = true;
 
 const Page = () => {
-  const rand = Math.floor(Math.random() * 99) + 1;
   return (
     <>
       <section className="pink_container pattern !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
-        <h1 className="heading">{rand}</h1>
       </section>
       <StartupForm />
     </>
