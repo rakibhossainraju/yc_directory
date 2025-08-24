ebd92f21e2286384ab98a6ef8ed94728995e65fe
rakibhossainraju <rakibhossianraju@gmail.com>
2025-08-24 23:15:59 +0600

    Removed all the unquery event listeners.

--- components/ProgressBarController.tsx
+++ components/ProgressBarController.tsx
@@ -8,15 +8,8 @@ export default function ProgressBarController() {
     const start = () => NProgress.start();
     const done = () => NProgress.done();
 
-    // Hard navigations away from this page
     window.addEventListener('beforeunload', start);
-
-    //When this page finishes initial load
-    if (document.readyState === 'complete') {
-      done();
-    } else {
-      window.addEventListener('load', done, { once: true });
-    }
+    window.addEventListener('load', done, { once: true });
 
     return () => {
       window.removeEventListener('beforeunload', start);
