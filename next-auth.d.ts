4a103314e55eeb088f5634a1d1c8c3cee84be607
rakibhossainraju <rakibhossianraju@gmail.com>
2026-01-04 15:14:55 +0600

    Update TypeScript definitions and dependencies: enhance session interface, upgrade Next.js and React packages, and refine tsconfig settings

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,7 +2,7 @@ import { type Session as AuthSession } from 'next-auth';
 import { type JWT as AuthJWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
-  interface Session extends AuthSession {
+  export interface Session extends AuthSession {
     id: string;
   }
   interface JWT extends AuthJWT {
