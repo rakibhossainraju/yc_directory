f731759f009f56f8bfa525590ba3d7da1b30d202
rakibhossainraju <rakibhossianraju@gmail.com>
2026-01-05 12:49:57 +0600

    Refactor UserDetailsSkeleton for improved loading visuals and structure

--- app/(root)/user/[id]/UserDetailsSkeleton.tsx
+++ app/(root)/user/[id]/UserDetailsSkeleton.tsx
@@ -1,21 +1,22 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import StartupCardSkeleton from '@/components/StartupCardSkeleton';
+
 export default function UserDetailsSkeleton() {
   return (
     <section className="profile_container">
       <div className="profile_card">
         <div className="profile_title">
-          <div className="h-6 bg-gray-300 rounded w-3/4 mx-auto animate-pulse" />
+          <Skeleton className="h-8 rounded w-3/4 mx-auto " />
         </div>
-        <div className="w-[200px] h-[300px] bg-gray-300 rounded mx-auto animate-pulse" />
-        <div className="h-8 bg-gray-300 rounded w-1/2 mx-auto mt-7 animate-pulse" />
-        <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto mt-1 animate-pulse" />
+        <Skeleton className="bg-slate-200 w-50 h-75 rounded-full mx-auto" />
+        <Skeleton className="bg-slate-200 w-full h-8 rounded mx-auto mt-7" />
+        <Skeleton className="bg-slate-200 h-4 rounded w-2/3 mx-auto mt-4" />
       </div>
       <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
-        <div className="h-8 bg-gray-300 rounded w-1/4 animate-pulse" />
-        <ul className="card_grid-sm">
-          <div className="h-48 bg-gray-300 rounded animate-pulse" />
-          <div className="h-48 bg-gray-300 rounded animate-pulse" />
-          <div className="h-48 bg-gray-300 rounded animate-pulse" />
-        </ul>
+        <Skeleton className="h-8 mb-2.5 rounded w-1/4" />
+        <div className="card_grid-sm">
+          <StartupCardSkeleton />
+        </div>
       </div>
     </section>
   );
