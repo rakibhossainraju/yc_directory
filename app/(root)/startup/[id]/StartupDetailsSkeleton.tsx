a7ad9ee37050035767c1594580c23e0b78e1a1bf
rakibhossainraju <rakibhossianraju@gmail.com>
2026-01-05 12:49:19 +0600

    Refactor StartupDetailsSkeleton for improved loading visuals and structure

--- app/(root)/startup/[id]/StartupDetailsSkeleton.tsx
+++ app/(root)/startup/[id]/StartupDetailsSkeleton.tsx
@@ -4,11 +4,15 @@ export const StartupDetailsSkeleton = () => {
   return (
     <>
       <section className="pink_container pattern !main-h-[230px]">
-        <Skeleton className="h-6 w-32 mb-4" />
-        <div className="heading">
-          <Skeleton className="h-10 w-full fade-in" />
+        <div className="tag">
+          <Skeleton className="bg-primary/30 h-3 w-24 px-2 my-1 rounded!" />
         </div>
-        <Skeleton className="h-6 w-full max-w-5xl fade-in mt-4" />
+        <div className="heading w-[50%] flex flex-col items-center gap-4 p-4!">
+          <Skeleton className="bg-pink-200/50 h-6 w-full rounded!" />
+          <Skeleton className="bg-pink-200/50 h-6 w-[70%] px-2 rounded!" />
+        </div>
+        <Skeleton className="bg-pink-200 h-5 w-[30%] max-w-5xl mt-4 rounded!" />
+        <Skeleton className="bg-pink-200 h-5 w-[20%] max-w-5xl mt-4 rounded!" />
       </section>
 
       <section className="section_container">
