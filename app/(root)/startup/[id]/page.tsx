cee49a546d82bf762591f3fb9dc9ff8b4688b7de
rakibhossainraju <rakibhossianraju@gmail.com>
2026-01-11 15:28:12 +0600

    Refactor startup data fetching to use getStartupsByQuery and remove unused caching logic

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,23 +1,8 @@
-import { cacheLife, cacheTag } from 'next/cache';
 import { Suspense } from 'react';
-
-import { sanityFetch } from '@/sanity/lib/live';
-import { STARTUP_DETAIL_QUERY } from '@/sanity/lib/queries';
 import { StartupDetails } from '@/app/(root)/startup/[id]/StartupDetails';
 import { StartupDetailsSkeleton } from '@/app/(root)/startup/[id]/StartupDetailsSkeleton';
 
 export type StartupParamType = Promise<{ id: string }>;
-
-export async function getStartupDetails(id: string) {
-  'use cache';
-  cacheTag('startup-details-' + id);
-  cacheLife('days');
-  const { data } = await sanityFetch({
-    query: STARTUP_DETAIL_QUERY,
-    params: { id },
-  });
-  return data;
-}
 
 const StartupDetailsPage = async ({ params }: { params: StartupParamType }) => {
   return (
