d1b3d1fd0cf8b3c6880424104f1801ba81461f30
rakibhossainraju <rakibhossianraju@gamil.com>
2026-01-12 22:39:02 +0600

    Refactor UserDetailsPage to remove Suspense and UserDetailsSkeleton, simplifying the component structure

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,14 +1,22 @@
-import { Suspense, ViewTransition } from 'react';
+import { ViewTransition } from 'react';
 import { UserDetails } from '@/app/(root)/user/[id]/UserDetails';
-import UserDetailsSkeleton from '@/app/(root)/user/[id]/UserDetailsSkeleton';
+import { redirect } from 'next/dist/server/api-utils';
+import { notFound } from 'next/navigation';
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+export function generateStaticParams() {
+  return [{ id: 'ZBFqyJCMiDLTwdwPrBOK9Y' }];
+}
+
+export default async function UserDetailsPage({ params }: PageProps<'/user/[id]'>) {
+  const { id } = await params;
+
+  if (!id) {
+    return notFound();
+  }
+
   return (
-    <Suspense fallback={<UserDetailsSkeleton />}>
-      <ViewTransition>
-        <UserDetails params={params} />
-      </ViewTransition>
-    </Suspense>
+    <ViewTransition>
+      <UserDetails userId={id} />
+    </ViewTransition>
   );
-};
-export default Page;
+}
