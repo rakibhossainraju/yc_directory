63dfa3d7dfd80e6409709ff922a478d111fdb9f9
rakibhossainraju <rakibhossianraju@gamil.com>
2026-01-12 22:39:28 +0600

    Refactor getUserDetailsById function to improve type safety and structure UserProfile interface

--- lib/queries/user.queries.ts
+++ lib/queries/user.queries.ts
@@ -2,15 +2,24 @@ import { cacheLife, cacheTag } from 'next/cache';
 import { clientFetch } from '@/sanity/lib/client';
 import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
 
+export interface UserProfile {
+  _id: string;
+  name: string;
+  username: string;
+  bio: string;
+  image: string;
+  startup_refs?: string[];
+}
+
 export async function getUserDetailsById(id: string) {
   'use cache';
   const tag = 'user-details-' + id;
   cacheTag(tag);
   cacheLife('days');
-
-  return await clientFetch({
+  const user = await clientFetch({
     query: AUTHOR_BY_ID_QUERY,
     params: { id },
     tags: [tag],
   });
+  return user as UserProfile | null;
 }
